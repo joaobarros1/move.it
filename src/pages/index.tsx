6898df2cee6e4d69b05b117488a4a5231c826c57
Joao Pedro Correia Barros <jpedrocbarros@gmail.com>
2021-02-28 01:07:31 -0300

    subindo

---
+++ src/pages/index.tsx
@@ -0,0 +1,34 @@
+import Head from 'next/head'
+
+import { CompletedChallenges } from "../components/CompletedChallenges";
+import { ExperienceBar } from "../components/ExperienceBar";
+import { Profile } from "../components/Profile";
+import { ChallengeBox } from "../components/ChallengeBox";
+import { Countdown } from "../components/Countdown"
+
+import styles from '../styles/pages/Home.module.css';
+import { CountdownProvider } from '../contexts/CountdownContext';
+
+export default function Home() {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Home | move.it</title>
+      </Head>
+
+      <ExperienceBar />
+      <CountdownProvider>
+        <section>
+          <div>
+            <Profile />
+            <CompletedChallenges />
+            <Countdown />
+          </div>
+          <div>
+            <ChallengeBox />
+          </div>
+        </section>
+      </CountdownProvider>
+    </div>
+  )
+}
